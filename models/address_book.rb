0149ac5c6b5a898129f923e7d18aa277c94cdc96
Alberto <alberto.grau.margarida@gmail.com>
2015-09-07 19:44:07 -0400

    Checkpoint-21-CSV

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry.rb'
+require "csv"
 
 class AddressBook
   attr_accessor :entries
@@ -8,25 +9,38 @@ class AddressBook
   end
 
   def remove_entry(name, phone_number, email)
-    delete_entry = nil    #don't understand this 'delete_entry' variable?
+    entry_to_delete = nil
 
-    @entries.each do |x|
-      if name == x.name && phone_number == x.phone_number && email == x.email
+    @entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        entry_to_delete = entry
+        break
       end
+    end
+
+    @entries.delete(entry_to_delete)
   end
 
-  @entries.delete(delete_entry)
-end
-
   def add_entry(name, phone_number, email)
-    index = 0     #Dont understand index?
+    index = 0
     @entries.each do |entry|
-
       if name < entry.name
         break
       end
-      index += 1  #?!?
+      index += 1
     end
-    @entries.insert(index, Entry.new(name, phone_number, email)) #??
+    @entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def import_from_csv(file_name)
+    csv_text = File.read(file_name) # Reads csv file
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    # The result of parse is an obj of type CSV::Table
+
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+
   end
 end
