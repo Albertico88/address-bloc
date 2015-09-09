3e6f6c79b7371da7130fb23223132857ab34996c
Alberto <alberto.grau.margarida@gmail.com>
2015-09-09 15:33:02 -0400

    checkpoint-22-search

--- models/address_book.rb
+++ models/address_book.rb
@@ -41,7 +41,7 @@ class AddressBook
     csv.each do |row|
       row_hash = row.to_hash
       # puts "Row to add: #{row_hash}"
-      # puts "Phone: #{row_hash[:phone_number].inspect}"
+      # Debug code-> puts "Phone: #{row_hash[:phone_number].inspect}"
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
   end
@@ -55,5 +55,27 @@ class AddressBook
         row_hash = row.to_hash
         add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
       end
-  end
+    end
+
+    def binary_search(name)
+      lower = 0
+   upper = entries.length - 1
+
+# #2
+   while lower <= upper
+# #3
+     mid = (lower + upper) / 2
+     mid_name = entries[mid].name
+
+# #4
+     if name == mid_name
+       return entries[mid]
+     elsif name < mid_name
+       upper = mid - 1
+     elsif name > mid_name
+       lower = mid + 1
+     end
+   end
+      return nil
+    end
 end
